240d2f17248a9da0ba6a375ab8fd5cacc0260910
websitealive <websitealive@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-15 10:31:49 +0000

    updated version
    git-svn-id: https://plugins.svn.wordpress.org/websitealive/trunk@1221491 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/login.php
+++ html/login.php
@@ -1,11 +1,11 @@
 <div id="div_wp_login" style="display:none; width:500px">
 
-	<h1>WebsiteAlive For Wordpress</h1>
+  <h1>WebsiteAlive For Wordpress</h1>
 
-	<div class="div_features">Easily add your WebsiteAlive live chat and click-to-call window to your Wordpress site.</div>
+  <div class="div_features">Easily add your WebsiteAlive live chat and click-to-call window to your Wordpress site.</div>
 
-	<p>To start, please enter your WebsiteAlive Administrator login details.</p>
-	<br />
+  <p>To start, please enter your WebsiteAlive Administrator login details.</p>
+  <br />
     
     <table border="0" cellpadding="4" cellspacing="4" align="center">
       <tr>
@@ -15,15 +15,15 @@
       <tr>
         <td class="aRight"><p>Password:</p></td>
         <td><input type="password" id="password" value=""/><br />
-        	
+          
         </td>
       </tr>
       <tr>
-      	<td></td>
+        <td></td>
         <td>
-        	<input type="button" onclick="validateLogin()" value="Log In"/>
+          <input type="button" onclick="validateLogin()" value="Log In"/>
             <br /><br />
-            <a href="#" onclick="$('#div_wp_login').hide();$('#div_wp_config').hide();$('#div_wp_register').show();$('#reg_username').focus()"><p class="fs11">New User? Sign Up Now</p></a>
+            <a href="#" onclick="$('#div_wp_login, #div_wp_config').hide();$('#div_wp_register').show();$('#reg_username').focus();"><p class="fs11">New User? Sign Up Now</p></a>
         </td>
       </tr>
     </table>
--- html/register.php
+++ html/register.php
@@ -1,11 +1,11 @@
 <div id="div_wp_register" style="display:none; width:500px">
 
-	<h1>New User Registration</h1>
+  <h1>New User Registration</h1>
 
-	<div class="div_features">Easily add your WebsiteAlive live chat and click-to-call window to your Wordpress site.</div>
+  <div class="div_features">Easily add your WebsiteAlive live chat and click-to-call window to your Wordpress site.</div>
 
-	<p>To sign up a new account, please enter a Username and your Email.</p>
-	<br />
+  <p>To sign up a new account, please enter a Username and your Email.</p>
+  <br />
     
     <table width="auto" border="0" cellpadding="4" cellspacing="4" align="center">
       <tr>
@@ -15,15 +15,15 @@
       <tr>
         <td class="aRight"><p>Email:</p></td>
         <td><input type="text" id="reg_emailadmin" value=""/><br />
-        	
+          
         </td>
       </tr>
       <tr>
-      	<td></td>
+        <td></td>
         <td>
-        	<input type="button" onclick="register()" value="Register"/>
+          <input type="button" onclick="register();" value="Register"/>
             <br /><br />
-            <p class="fs11"><a href="#" onclick="$('#div_wp_login').show();$('#div_wp_config').hide();$('#div_wp_register').hide();$('#user_name').focus()"><p class="fs11">Existing User?</a></p> 
+            <p class="fs11"><a href="#" onclick="$('#div_wp_login, #div_wp_config').hide();$('#div_wp_register').hide();$('#user_name').focus();"><p class="fs11">Existing User?</a></p> 
         </td>
       </tr>
     </table>
